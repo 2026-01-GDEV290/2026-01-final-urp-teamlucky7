579f472fb2731c73fce5ea58ce6c4f985aba6e73
LTate12 <157613144+LTate12@users.noreply.github.com>
2026-03-19 11:49:35 -0400

    leaderboard work

--- Assets/Leaderboard.cs
+++ Assets/Leaderboard.cs
@@ -50,6 +50,20 @@ public class Leaderboard : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        InitializeLeaderboard();
+        ShowLeaderboard();
+
+        Debug.Log(npcPool.Count);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InitializeLeaderboard()
+    {
         npcPool.Add("Strangula", 0);
         npcPool.Add("Bullseye", 0);
         npcPool.Add("Madam Fortune", 0);
@@ -65,40 +79,64 @@ public class Leaderboard : MonoBehaviour
                 npcPool.Remove(playerCharacter);
             }
 
-            for (int i = 0; i <= npcPool.Count; i++)
+            for (int i = 0; i < npcPool.Count - 1; i++)
             {
                 int npcMoneyValue = Random.Range(50, 6000);
                 npcPool[npcList[i]] = npcMoneyValue;
             }
-        } else
+        }
+        else
         {
-
+            for (int i = 0; i < npcPool.Count - 1; i++)
+            {
+                int npcMoneyValue = Random.Range(150, 5000);
+                npcPool[npcList[i]] += npcMoneyValue;
+            }
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void showLeaderboard()
+    public void ShowLeaderboard()
     {
         string[] npcOrderedList = {"", "", "", "", "", "", ""};
         float[] npcOrderedValue = { 0, 0, 0, 0, 0, 0, 0 };
 
-        for (int i = 0; i < npcPool.Count; i++)
+        for (int i = npcPool.Count; i > 0; i--)
         {
-            if (npcPool[npcList[i]] < npcPool[npcList[i - 1]])
+            for(int j = 0; j < npcOrderedValue.Length; j++)
             {
-                npcOrderedList[i - 1] = npcList[i];
-                npcOrderedValue[i -1] = npcPool[npcList[i]];
-            } else
-            {
-                npcOrderedList[i] = npcList[i];
-                npcOrderedValue[i] = npcPool[npcList[i]];
+
             }
         }
+        //for (int i = 0; i < npcPool.Count - 1; i++)
+        //{
+        //    if(i == 0)
+        //    {
+        //        npcOrderedList[i] = npcList[i];
+        //    } else
+        //    if (!npcPool.ContainsKey(npcList[i]))
+        //    {
+        //        if (playerMoneyValue < npcPool[npcList[i - 1]])
+        //        {
+        //            npcOrderedList[i] = npcList[i - 1];
+        //            npcOrderedValue[i - 1] = playerMoneyValue;
+        //        }
+        //        else
+        //        {
+        //            npcOrderedList[i] = npcList[i];
+        //            npcOrderedValue[i] = playerMoneyValue;
+        //        }
+        //    }
+        //    else if (npcPool.ContainsKey(npcList[i]) && npcPool[npcList[i]] < npcPool[npcList[i - 1]])
+        //    {
+        //        npcOrderedList[i - 1] = npcList[i];
+        //        npcOrderedValue[i - 1] = npcPool[npcList[i]];
+        //    }
+        //    else
+        //    {
+        //        npcOrderedList[i] = npcList[i];
+        //        npcOrderedValue[i] = npcPool[npcList[i]];
+        //    }
+        //}
 
         npcText1.text = npcOrderedList[0];
         npcText2.text = npcOrderedList[1];
@@ -108,6 +146,28 @@ public class Leaderboard : MonoBehaviour
         npcText6.text = npcOrderedList[5];
         npcText7.text = npcOrderedList[6];
 
-        npcValue1.text = npcOrderedValue[0];
+        npcValue1.text = "" + npcOrderedValue[0];
+        npcValue2.text = "" + npcOrderedValue[1];
+        npcValue3.text = "" + npcOrderedValue[2];
+        npcValue4.text = "" + npcOrderedValue[3];
+        npcValue5.text = "" + npcOrderedValue[4];
+        npcValue6.text = "" + npcOrderedValue[5];
+        npcValue7.text = "" + npcOrderedValue[6];
+
+        npcText1.enabled = true;
+        npcText2.enabled = true;
+        npcText3.enabled = true;
+        npcText4.enabled = true;
+        npcText5.enabled = true;
+        npcText6.enabled = true;
+        npcText7.enabled = true;
+
+        npcValue1.enabled = true;
+        npcValue2.enabled = true;
+        npcValue3.enabled = true;
+        npcValue4.enabled = true;
+        npcValue5.enabled = true;
+        npcValue6.enabled = true;
+        npcValue7.enabled = true;
     }
 }
