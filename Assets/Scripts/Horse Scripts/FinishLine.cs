8c946b4db65fc4ef3ed46fe11b93c6cd7385bbc0
Cric1221 <chrisrok2000@gmail.com>
2026-04-30 01:05:16 -0400

    fixed horse racing
    horse racing finish line now detects when horse crosses

--- Assets/Scripts/Horse Scripts/FinishLine.cs
+++ Assets/Scripts/Horse Scripts/FinishLine.cs
@@ -7,26 +7,30 @@ public class FinishLine : MonoBehaviour
     public BettingManager bettingManager; // Drag BettingManager here
     private bool raceOver = false;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter(Collider other)
     {
-        if (!raceOver && other.CompareTag("Horse"))
-        {
-            raceOver = true;
-            string winner = other.gameObject.name;
-            winText.gameObject.SetActive(true);
+            Debug.Log("Something entered finish line: " + other.name);
 
-            // Check if player picked this horse
-            if (winner == bettingManager.pickedHorseName)
+            if (!raceOver && other.CompareTag("Horse"))
             {
-                winText.text = winner + " Won! You won $" + (bettingManager.currentBetAmount * 2);
-                bettingManager.AddWinnings(2f); // 2x Payout for a win
-            }
-            else
-            {
-                winText.text = winner + " Won. You lost your bet.";
-            }
+                raceOver = true;
 
-            FindObjectOfType<RaceManager>().StopAllHorses();
-        }
+                string winner = other.gameObject.name;
+                winText.gameObject.SetActive(true);
+
+                if (winner == bettingManager.pickedHorseName)
+                {
+                    winText.text = winner + " Won! You won $" + (bettingManager.currentBetAmount * 2);
+                    bettingManager.AddWinnings(2f);
+                }
+                else
+                {
+                    winText.text = winner + " Won. You lost your bet.";
+                }
+
+                FindObjectOfType<RaceManager>().StopAllHorses();
+
+                Debug.Log("Horse crossed finish line!");
+            }
     }
 }
