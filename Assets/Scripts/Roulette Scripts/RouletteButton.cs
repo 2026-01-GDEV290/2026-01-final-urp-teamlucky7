1edbcb6333a18463d5a89bba01d9bc3b266da4e6
Cric1221 <chrisrok2000@gmail.com>
2026-05-06 22:55:54 -0400

    Reduced filesto only web build

--- Assets/Scripts/Roulette Scripts/RouletteButton.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class RouletteButton : MonoBehaviour
-{
-    public string betNumber; // "1", "17", "0", "00"
-    public bool isSpecialBet;
-    public RouletteBetType specialBetType = RouletteBetType.Invalid;
-}
